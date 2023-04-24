68d6d72ac9befb93961d2a544996a7a8f6ce170f
Utkarsh Rastogi <95937956+URRG@users.noreply.github.com>
2023-04-24 23:24:00 +0530

    Update largestnum.java

--- largestnum.java
+++ largestnum.java
@@ -7,4 +7,13 @@ double a, b, c, largest, temp;
 Scanner sc = new Scanner(System.in);  
 System.out.println("Enter the first number:");  
 a = sc.nextDouble();  
+System.out.println("Enter the second number:");  
+b = sc.nextDouble();  
+System.out.println("Enter the third number:");  
+c = sc.nextDouble();  
+temp=a>b?a:b;  
+largest=c>temp?c:temp;  
+System.out.println("The largest number is: "+largest);  
+}  
+}  
 
